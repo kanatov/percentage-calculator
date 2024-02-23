2d51a728c9bc115066862c6e111153318957a8d9
Kanatov <ilya.kanatov@gmail.com>
2024-02-23 15:12:52 +0000

    Form values removed and repeatable elements templates were made

--- js/calculator.js
+++ js/calculator.js
@@ -6,8 +6,7 @@ class Calculator {
         this.plans = plans;
         // this.arr = [];
         this.monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-        console.log(this.dom);
-        console.log(this.plans);
+        this.render();
     }
 
 
@@ -108,73 +107,18 @@ class Calculator {
 
         return calc;
     }
-
+*/
     // Отрисовываем массив объектов arr
     render() {
-        var i;
-        var summ = 0;
 
-        list.innerHTML = '';
-
-        // Печатаем массив
-        for (i = this.arr.length - 1; i >= 0; i--) {
-            var h = '';
-
-            summ += this.calc(this.arr[i].summ, this.arr[i].date).s;
-
-            h += '<li>';
-            h += '<a class="del" href="#" onClick="calc.remove(' + i + ')">×</a>';
-            h += '<div class="cells">';
-            h += '<p class="cellLabel">Deposit amaunt</p><p class="value">';
-            h += this.arr[i].summ;
-            h += '</p>';
-            h += '<p class="cellLabel">Deposit date</p><p class="value">';
-            h += this.arr[i].date.getDate();
-            h += ' ';
-            h += this.monthNames[this.arr[i].date.getMonth()];
-            h += ' ';
-            h += this.arr[i].date.getFullYear();
-            h += '</p></div>';
-
-            h += '<div class="cells">';
-            h += '<p class="cellLabel">The maturity amount today</p>';
-            h += '<p class="cellLabel">The maturity amount in ';
-            h += this.calc(this.arr[i].summ, this.arr[i].date).d;
-            h += ' days';
-            h += '<p class="cellLabel">Amount of payouts</p>';
-            h += '</div>';
-
-            h += '<div class="cells">';
-            h += '<p class="value"><b>';
-            h += this.calc(this.arr[i].summ, this.arr[i].date).s;
-            h += '</b></p>';
-            h += '<p class="value">';
-            h += this.calc(this.arr[i].summ, this.arr[i].date).sn;
-            h += '</p><p class="value">';
-            h += this.calc(this.arr[i].summ, this.arr[i].date).m;
-            h += '</p>';
-            h += '</div>';
-            h += '</li>';
-
-            list.innerHTML += h;
-        }
-
-        if (this.arr.length > 1) {
-            summValue.innerHTML = summ.toFixed(2);
-            summLabel.style.display = 'inherit';
-            summValue.style.display = 'inherit';
-        } else {
-            summLabel.style.display = 'none';
-            summValue.style.display = 'none';
-        }
     }
-
-    notify(msg) {
-        if (msg) {
-            this.dom.summ.style.borderColor = 'red';
-        } else {
-            this.dom.summ.style.borderColor = '';
+    /*
+        notify(msg) {
+            if (msg) {
+                this.dom.summ.style.borderColor = 'red';
+            } else {
+                this.dom.summ.style.borderColor = '';
+            }
         }
-    }
-    */
+        */
 }
